b19157a27a595d032b0cf3c90e908117eea323a8
Valentin Bonnal <co.merce@hotmail.fr>
2015-11-27 17:49:14 +0100

    Ajout Test Unitaire

---
+++ Test/ModelUnitTest.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+
+/**
+ * Created by Valentin on 27/11/2015.
+ */
+public class ModelUnitTest {
+
+	@Test
+	public void jetteTest() {
+		Model model = new Model();
+		model.jette();
+	}
+}
